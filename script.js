4ba1476bf8138cd651a71ea95db64b854a17a196
Sona <sona_p86@list.ru>
2023-02-02 21:23:11 +0300

    step1

---
+++ script.js
@@ -0,0 +1,9 @@
+/*Задание 1*/
+let a = 25;
+let b = (9/5)*a+32;
+console.log(`${a} гадусов  по Цельсии равно ${b} градусу по Фаренгейту` );
+
+/*Задание 2*/
+let name = "Sona";
+let admin = name
+console.log(admin);
