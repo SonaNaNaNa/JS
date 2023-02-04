896e3e7c05f798541872cb290aceecbfbab276b6
Sona <sona_p86@list.ru>
2023-02-04 21:03:54 +0300

    step2

---
+++ script2.js
@@ -0,0 +1,26 @@
+//Задание 1
+/*let num1 = Number(prompt('Введите число 1:'));
+let num2 = Number(prompt('Введите число 2:'));
+( num1<=1) ? console.log('true') : console.log('false'); 
+( num2>=3) ? console.log('true') : console.log('false');*/
+
+//Задание 2
+/*let test = true;
+(test === true) ? console.log('+++') : console.log('---');*/
+
+//Задание 3
+/*let day = Math.round(Math.random()*31);
+if(day<=10){
+    console.log(`${day} число - это первая декада месяца`);
+}else if(day> 10 && day<=20){
+    console.log(`${day} число - это вторая декада месяца`); 
+}else{
+    console.log(`${day} число - это третья декада месяца`);
+}*/
+
+//Задание 4
+/*let userNumber = Number(prompt('Введите трехзначное число:'));
+let unit = userNumber%10;
+let ten = Math.floor((userNumber/10)%10);
+let hundred = Math.floor((userNumber/100)%10);
+console.log(`В числе ${userNumber} ${hundred} сотен, ${ten} десятков и ${unit} единиц. `);*/
