e48cec4d0a311bdef7615e201ad3b7da74c37500
Sona <sona_p86@list.ru>
2023-02-09 19:44:37 +0300

    step4

---
+++ script3.js
@@ -0,0 +1,69 @@
+//Задание 1
+//Создайте функцию которая возводит переданное число в куб, необходимо вывести в консоль результат 2^3 степени + 3 ^ 3 степени
+
+/*function cub(param) {
+    let result = param*param*param;
+    return result;
+}
+console.log(cub(2)+cub(3));*/
+
+//Задание 2 
+/*function tax(param) {
+    let result = param*0.87;
+    return result;
+}
+let userNumber = Number(prompt('Введите размер заработной платы' , 100000));
+if  (isNaN(userNumber)) {
+    alert('Вы ввели неправильное значение');
+} else{
+    console.log(`Размер заработной платы за вычетом налогов равен ${tax(userNumber)}`);
+}*/
+
+//Задание 3
+
+/*function Maxim(a, b, c) {
+    if (a>b && a>c) {
+      console.log(`Максимальное значение ${a}`);  
+    } else if (b>a && b>c) {
+        console.log(`Максимальное значение ${b}`);  
+    } else {
+        console.log(`Максимальное значение ${c}`);
+    }
+}
+let num1 = Number(prompt('Введите 1 число'));
+let num2 = Number(prompt('Введите 2 число'));
+let num3 = Number(prompt('Введите 3 число'));
+console.log(Maxim(num1, num2, num3));*/
+
+//Задание 4
+/*function sum(num1,num2) {
+    let result = num1+num2;
+    return result;
+}*/
+
+/*function difference(num1, num2) {
+    if (num1>num2) {
+        let diff = num1-num2;
+        return diff;
+    } else {
+        let diff1 = num2-num1;
+        return diff1;
+    }
+}*/
+
+/*function multiplication(num1, num2) {
+    let mult = num1*num2;
+    return mult;
+}*/
+
+/*function div(num1, num2) {
+    if (num1>num2) {
+        let div1 = num1/num2;
+        return div1;
+    } else {
+        let div2 = num2/num1;
+        return div2;
+    }
+}*/
+//console.log(div(2,40));
+//console.log(difference(15, 96));
